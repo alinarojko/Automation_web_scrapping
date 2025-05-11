7487dc144666ac55de4e41c39d24e471b2cf8f4f
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-05-11 19:16:12 +0100

    Login functionality automated

--- login_page.py
+++ login_page.py
@@ -1,9 +1,10 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+
 
 def get_driver():
     # Set option to make browsing easier
@@ -16,21 +17,19 @@ def get_driver():
     options.add_argument("disable-blink-features=AutomationControlled")  # to avoid detection of the browsers
 
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.get("http://automated.pythonanywhere.com")
+    driver.get("http://automated.pythonanywhere.com/login/")
     return driver
 
 
-def clean_text(text):
-    """Extract only the temperature from text"""
-    output = float(text.split(": ")[1])
-    return output
-
-
-def main():
+def login():
     driver = get_driver()
+    driver.find_element(By.ID, "id_username").send_keys("automated")
     time.sleep(2)
-    element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[2]")
-    return clean_text(element.text)
+    driver.find_element(By.ID, "id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//a[@class='navbar-brand']").click()
+    print(driver.current_url)
 
-print(main())
+
+print(login())
 
