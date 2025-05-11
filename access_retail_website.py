341b09075afbfe26ba374434900aae93e7e68a5e
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-05-11 20:06:26 +0100

    Automated login and contact us form on the retail website

--- access_retail_website.py
+++ access_retail_website.py
@@ -1,9 +1,9 @@
 import time
-from datetime import datetime as dt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+
 
 def get_driver():
     # Set option to make browsing easier
@@ -16,33 +16,24 @@ def get_driver():
     options.add_argument("disable-blink-features=AutomationControlled")  # to avoid detection of the browsers
 
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.get("http://automated.pythonanywhere.com")
+    driver.get("https://titan22.com/account/login?return_url=%2Faccount")
     return driver
 
 
-def clean_text(text):
-    """Extract only the temperature from text"""
-    output = float(text.split(": ")[1])
-    return output
-
-
-def save_value_to_file(text):
-    """Write input text into a file"""
-    filename = f"{dt.now().strftime("%Y-%m-%d.%H-%M-%S")}.txt"
-    with open("files/filename", "w") as file:
-        file.write(text)
-
-
-def main():
+def login():
     driver = get_driver()
-    while True:
-        time.sleep(2)
-        element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[2]")
-        text = str(clean_text(element.text))
-        save_value_to_file(text)
+    driver.find_element(By.XPATH,"//input[@id='CustomerEmail']").send_keys("ipr.1776@gmail.com")
+    time.sleep(2)
+    driver.find_element(By.XPATH,"//input[@id='CustomerPassword']").send_keys("testuser")
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//button[normalize-space()='Sign In']").click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//a[normalize-space()='Contact Us']").click()
+    return driver.current_url
+
+print(login())
 
 
-print(main())
 
 
 
